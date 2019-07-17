a5457319569f07677dd5c58bf0fe9222c8ef63ff
edulop <elopez@chanzuckerberg.com>
2019-07-17 13:34:24 -0700

    Also add an interval here

--- main.go
+++ main.go
@@ -19,33 +19,38 @@ func main() {
 }
 
 func run() error {
-	timeout, command, args, err := parseArguments()
+	timeout, interval, command, args, err := parseArguments()
 	if err != nil {
 		return err
 	}
-	return travisWait(timeout, command, args...)
+	return travisWait(timeout, interval, command, args...)
 }
 
-func parseArguments() (time.Duration, string, []string, error) {
+func parseArguments() (time.Duration, time.Duration, string, []string, error) {
 	timeoutString := flag.String("timeout", "20m", "Timeout for this command")
+	intervalString := flag.String("interval", "1m", "The interval at which to print keep-alive messages")
 
 	flag.Parse()
 	args := flag.Args()
 
 	timeout, err := time.ParseDuration(*timeoutString)
 	if err != nil {
-		return timeout, "", nil, errors.Wrap(err, "could not parse timeout as a duration")
+		return timeout, 0, "", nil, errors.Wrap(err, "could not parse timeout as a duration")
+	}
+	interval, err := time.ParseDuration(*intervalString)
+	if err != nil {
+		return timeout, interval, "", nil, errors.Wrap(err, "could not parse interval as duration")
 	}
 
 	if len(args) < 1 {
-		return timeout, "", nil, errors.New("could not parse command to run")
+		return timeout, interval, "", nil, errors.New("could not parse command to run")
 	}
 
-	return timeout, args[0], args[1:], nil
+	return timeout, interval, args[0], args[1:], nil
 }
 
-func travisWait(timeout time.Duration, command string, args ...string) error {
-	ticker := time.NewTicker(time.Minute)
+func travisWait(timeout time.Duration, interval time.Duration, command string, args ...string) error {
+	ticker := time.NewTicker(interval)
 	go func() {
 		for t := range ticker.C {
 			logrus.Infof("go-travis-wait waiting at %s...", t.Format(time.RFC1123Z))
